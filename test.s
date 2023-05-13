ccd4e4f68ec08a1617371ed04c584b30b897dd9f
RaidenYing <hurryBoyha@gmail.com>
2023-05-13 15:27:20 +0800

    修复了新增ret指令而行数不增加的bug

--- test.s
+++ test.s
@@ -1,6 +1,3 @@
-.data
-    v0:
-        .space 80000
 .globl main
 .text
 main:
@@ -8,18 +5,20 @@ main:
     sd ra, 8(sp)
     sd fp, 0(sp)
     addi fp, sp, 16
-    li t0, 1
-    li t1, 2
-    li t2, 8
-    mul t3, t1, t2
-    li t1, 1
-    li t2, 8
-    mul t4, t1, t2
-    la t1, v0
-    add t2, t1, t4
-    ld t2, 0(t2)
-    add t1, t2, t3
-    mv t1, t0
+    jal getint
+    mv t0, a0
+    addi sp, sp, -8
+    sd t0, -16(fp)
+    jal getint
+    mv t0, a0
+    addi sp, sp, -8
+    sd t0, -24(fp)
+    ld t0, -16(fp)
+    mv a0, t0
+    jal putint
+    ld t0, -24(fp)
+    mv a0, t0
+    jal putint
     li t0, 0
     mv sp, fp
     ld ra, -8(fp)
